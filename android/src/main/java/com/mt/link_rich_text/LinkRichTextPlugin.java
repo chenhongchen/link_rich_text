03d3be28c805a327df376a5362cff62d3b04ca22
chenhongchen <412130100@qq.com>
2022-05-13 16:04:29 +0800

    添加flutter版本限制

--- android/src/main/java/com/mt/link_rich_text/LinkRichTextPlugin.java
+++ android/src/main/java/com/mt/link_rich_text/LinkRichTextPlugin.java
@@ -16,20 +16,6 @@ public class LinkRichTextPlugin implements FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(new LinkRichTextPlugin());
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "link_rich_text");
-    channel.setMethodCallHandler(new LinkRichTextPlugin());
-  }
-
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
